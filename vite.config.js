af88ea6c6154a6a33ee3a7d0e8fe4ce56b70b5e0
MD. Azharul Islam <113032991+sazharul@users.noreply.github.com>
2024-11-11 02:37:33 +0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+    base: '/', // Base path for development and production
+    root: path.resolve(__dirname, 'src'), // Set the root to 'src'
+    publicDir: path.resolve(__dirname, 'public'), // Set the public directory for static files
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'), // Alias for bootstrap
+        },
+    },
+    server: {
+        port: 8080, // Development server port
+        hot: true,  // Enable HMR (Hot Module Replacement)
+        open: true, // Automatically open in the browser
+    },
+    build: {
+        outDir: path.resolve(__dirname, 'dist'), // Output directory for the production build
+        emptyOutDir: true, // Clean the output directory before building
+        rollupOptions: {
+            input: {
+                main: path.resolve(__dirname, 'src', 'index.html'), // Specify the main entry point
+            },
+        },
+    },
+});
